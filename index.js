6a821368ea52e3ce084b2c0f4456b7af808c968a
walk8243 <walk8243@gmail.com>
2018-03-03 20:00:46 +0900

    http-authによるログインの設定

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var express = require('express');
+var express = require('express'),
+    auth    = require('http-auth');
 
-var app = express();
+var app   = express(),
+    admin = express();
+
+app.use('/admin', admin);
+
+var basic = auth.basic({
+  realm: "aaa"
+},function(username, password, callback){
+  callback(username === "Tina" && password === "Bullock");
+});
 
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!');
@@ -9,4 +19,9 @@ app.listen(3000, function(){
 app.route('/')
   .get(function(req, res){
     res.end('Hello World!');
+  });
+
+admin.route('/')
+  .get(auth.connect(basic), function(req, res){
+    res.end('Welcome to private area - ' + req.user);
   });

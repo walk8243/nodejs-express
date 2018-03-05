230ba188ca3dade67b47bfcb7e3df90db09031e0
walk8243 <walk8243@gmail.com>
2018-03-05 22:49:25 +0900

    commit

--- createServer.js
+++ createServer.js
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-var fs      = require('fs'),
-    yaml    = require('js-yaml');
-
-var error   = require('./error.js');
-
-const configFile = './config/default.yml';
-try{
-  // configファイルの読み込み
-  config = yaml.safeLoad(fs.readFileSync(configFile, 'utf8'));
-  // console.log(config);
-  var server = config.setting.server;
-  // console.log(server);
-
-  block_createServer: {
-    if(process.argv.length > 2){
-      if(process.argv[2].match(/^[A-Za-z0-9]{3,10}$/)){
-        servername = process.argv[2];
-      }else{
-        console.log(error.printErrorMessage(2, [process.argv[2]]));
-        break block_createServer;
-      }
-    }else{
-      servername = 'create';
-    }
-    newServer = {
-      name: servername,
-      subdomain: [servername],
-      route: `${servername}.yml`
-    };
-    // 作成するホストの名前の検証
-    for(var host of server){
-      // console.log(host);
-      if(host.name == newServer.name){
-        console.log(error.printErrorMessage(1, [newServer.name]));
-        break block_createServer;
-      }
-    }
-    server.push(newServer);
-    console.log(newServer);
-    // console.log(server);
-    // console.log(config);
-
-    // 設定情報をyaml型に変換
-    dumpYaml = yaml.safeDump(config, {
-      'styles': {
-        '!!null': 'canonical'
-      }
-    });
-    // console.log(dumpYaml);
-
-    // configファイルに書き込み
-    fs.writeFile(configFile, dumpYaml, function(err){
-      if(err){throw err;}
-      console.log(`config file('${configFile}') has been saved!`);
-    });
-  }
-}catch(err){
-  console.log(err);
-}
-=======
 var fs      = require('fs'),
     yaml    = require('js-yaml');
 
@@ -201,4 +140,3 @@ function rmdir(path){
   fs.rmdirSync(path);
   // console.log(`${path} was deleted`);
 }
->>>>>>> refs/remotes/origin/master
--- error.js
+++ error.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-function printErrorMessage(mode, contents){
-  var message;
-  switch (mode) {
-    case 1:
-      message = `
-Server'${contents[0]}' is Already exist!
-Please choose a different name!
-      `;
-      break;
-    case 2:
-      message = `
-Hostname'${contents[0]}' is not appropriate!
-3 to 10 alphanumeric characters only.
-Please enter a different name!
-      `;
-      break;
-    default:
-  }
-
-  return message.replace(/^\n/, '');
-}
-
-module.exports.printErrorMessage = printErrorMessage;
-=======
 function printErrorMessage(mode, contents){
   var message;
   switch (mode) {
@@ -52,4 +27,3 @@ Please change the name of '${contents[0]}' or enter a different name!
 }
 
 module.exports.printErrorMessage = printErrorMessage;
->>>>>>> refs/remotes/origin/master

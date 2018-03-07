b1e927b3eb84d01418d8c997928b9756b96a13bc
walk8243 <walk8243@gmail.com>
2018-03-07 20:05:29 +0900

    Pageクラスで定義していたrenderをoverrideして、読み込んだデータをテンプレートに送信

--- page/main/index.js
+++ page/main/index.js
@@ -1,4 +1,5 @@
 const Page = require(process.cwd() + '/page.js');
+var yaml  = require('js-yaml');
 
 class Index extends Page {
   constructor(){
@@ -6,6 +7,16 @@ class Index extends Page {
 
     // 書き加える場合はここから
     // this.template = 'ddd';
+  }
+
+  render(res, data){
+    this.pageData.param = data;
+
+    var sendData = yaml.safeLoad(fs.readFileSync(process.cwd() + '/data/sample.yml', 'utf8'));
+    // console.log(sendData);
+    this.pageData = Object.assign(this.pageData, sendData);
+
+    myFunc.renderEjs(res, this.htmlStr, this.pageData);
   }
 }
 
